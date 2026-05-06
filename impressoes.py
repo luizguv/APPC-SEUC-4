87ac8ade9c89f9541afcdfaa291802716812a7eb
Luiz Gustavo <luiz.guv@puccampinas.edu.br>
2026-05-06 11:23:48 -0300

    Create impressoes.py

---
+++ impressoes.py
@@ -0,0 +1,6 @@
+import contas
+import seuc
+
+print(f"A porcentagem de leituras feitas na zona verde é {contas.porc:.2f}%.")
+print(f"A média das pressões já ajustadas é de: {contas.media:.2f} UPC.")
+print(f"Menor pressão registrada após o ajuste é: {contas.menor:.2f} UPC.")
